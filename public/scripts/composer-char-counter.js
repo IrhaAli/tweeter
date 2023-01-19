5ce43fa7972d1f472f0f1cbed264383df64e2449
Irha Ali <irhaali_97@hotmail.ca>
2023-01-18 20:55:13 -0500

    Add character counter feature

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,9 @@
-// $(document).ready(function() {
-//   // --- our code goes here ---
-//   $("tweet-text").on("change", () => {
-//     const remaining = 140 - $(this).text().length;
-//     $("number-of-character-field").value(remaining);
-//   });
-// });
+$(document).ready(function() {
+  console.log("Ready!");
+  $("textarea").on("input", function() {
+    const MAX_CHAR = 140;
+    const numChar = document.getElementById('num-of-char');
+    const remaining = MAX_CHAR - $(this).val().length;
+    numChar.textContent = remaining;
+  });
+});
